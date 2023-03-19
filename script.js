c3563abe2d63e90fd7daf120bc53607aa5454614
0xRomRom <roman0811@live.nl>
2023-03-19 11:38:37 +0100

    added current time to navbar

---
+++ script.js
@@ -0,0 +1,19 @@
+const currentTimeText = document.querySelector(".current-time");
+
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+// Convert hours to 12-hour format
+let amOrPm = hours >= 12 ? "PM" : "AM";
+hours = hours % 12;
+hours = hours ? hours : 12; // Handle midnight (0 hours)
+
+// Add leading zero to minutes if necessary
+minutes = minutes < 10 ? "0" + minutes : minutes;
+
+// Format the time as a string in PM format
+let timeInPMFormat = hours + ":" + minutes + " " + amOrPm;
+
+currentTimeText.textContent = timeInPMFormat;
+console.log(timeInPMFormat); // Output: "3:30 PM" (assuming the current time is 3:30 PM)
