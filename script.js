718c01bd204ded4cdc855f4189d41bfe916a7557
0xromrom <roman0811@live.nl>
2023-03-19 11:04:08 -0700

    Item drag working and not conflicting with nav bar

--- script.js
+++ script.js
@@ -45,3 +45,73 @@ barBottomBox.addEventListener("click", (e) => {
     startButton.classList.remove("clicked");
   }
 });
+
+// Drag desktop icons
+const wrapper = document.querySelector(".bar-bottom");
+// const desktopItem = document.querySelector(".desktop-icon");
+
+// function onDrag({ movementX, movementY }) {
+//   let getStyle = window.getComputedStyle(wrapper);
+//   let left = parseInt(getStyle.left);
+//   let top = parseInt(getStyle.top);
+//   wrapper.style.left = `${left + movementX}px`;
+//   wrapper.style.top = `${top + movementY}px`;
+// }
+
+// desktopItem.addEventListener("mousedown", () => {
+//   desktopItem.classList.add("active");
+//   desktopItem.addEventListener("mousemove", onDrag);
+// });
+
+// desktopItem.addEventListener("mouseup", () => {
+//   desktopItem.classList.remove("active");
+//   desktopItem.removeEventListener("mousemove", onDrag);
+// });
+
+const myDiv = document.querySelector(".desktop-icon");
+
+let isDragging = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+
+myDiv.addEventListener("mousedown", dragStart);
+
+function dragStart(e) {
+  initialX = e.clientX - xOffset;
+  initialY = e.clientY - yOffset;
+  document.addEventListener("mousemove", drag);
+  document.addEventListener("mouseup", dragEnd);
+  isDragging = true;
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
+
+  isDragging = false;
+
+  document.removeEventListener("mousemove", drag);
+  document.removeEventListener("mouseup", dragEnd);
+}
+
+function drag(e) {
+  if (isDragging) {
+    e.preventDefault();
+
+    currentX = e.clientX - initialX;
+    currentY = e.clientY - initialY;
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTransform(currentX, currentY, myDiv);
+  }
+}
+
+function setTransform(xPos, yPos, el) {
+  el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+}
