c8caaca98c8f453af4117db61233d2df731810cc
0xRomRom <roman0811@live.nl>
2023-03-29 21:06:44 +0200

    commit

--- js/modalCtaButtons.js
+++ js/modalCtaButtons.js
@@ -57,35 +57,3 @@ enlargeShadowMinter.addEventListener("click", () => {
   shadowMintModal.classList.remove("enlarged");
   shadowMinterEnlarged = false;
 });
-
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-
-canvas.addEventListener("mousedown", (event) => {
-  isDrawing = true;
-  lastX = event.offsetX;
-  lastY = event.offsetY;
-});
-
-canvas.addEventListener("mousemove", (event) => {
-  if (isDrawing) {
-    context.beginPath();
-    context.moveTo(lastX, lastY);
-    context.lineTo(event.offsetX, event.offsetY);
-    context.stroke();
-    lastX = event.offsetX;
-    lastY = event.offsetY;
-  }
-});
-
-canvas.addEventListener("mouseup", () => {
-  isDrawing = false;
-});
-
-canvas.addEventListener("mouseout", () => {
-  isDrawing = false;
-});
--- js/script.js
+++ js/script.js
@@ -172,3 +172,17 @@ socialsHover.addEventListener("mouseover", () => {
 socialsHover.addEventListener("mouseleave", () => {
   socialsMenu.classList.add("hidden");
 });
+
+const terminalInput = document.querySelector(".terminal-input");
+const terminalOutput = document.querySelector(".terminal-output");
+
+terminalInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    const command = e.target.value;
+    e.target.value = "";
+
+    const output = document.createElement("div");
+    output.textContent = command;
+    terminalOutput.appendChild(output);
+  }
+});
