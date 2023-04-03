70f2ac07070478858d0663b262ad12d831f1359e
Roman - <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>
2023-04-03 22:35:59 +0200

    started working on the minter modal

---
+++ js/shadowMinter.js
@@ -0,0 +1,41 @@
+// import contractABI from "./abi.js";
+
+const connectMetamask = document.querySelector(".connect-metamask");
+const connectUserWallet = document.querySelector(".connected-wallet");
+const modal1 = document.querySelector(".minter-inner-1");
+const modal2 = document.querySelector(".minter-inner-2");
+
+let account;
+let contractInstance;
+// const ABI = contractABI;
+const CONTRACT = "0xac084F5db68ee0Ba80AeCA734DA9AFD128F864d7";
+
+const connectToMetamask = async () => {
+    //Check if metamask is installed
+    if (window.ethereum) {
+        alert("eth!");
+        //Request current user
+        await window.ethereum.send("eth_requestAccounts");
+        
+        //Initialize web3 class
+        window.web3 = new Web3(window.ethereum);
+        
+        //Get array of accounts
+        const accounts = await web3.eth.getAccounts();
+        
+        //Select first account
+        account = accounts[0];
+        
+        //Display user wallet
+        modal1.classList.add("hidden");
+        modal2.classList.remove("hidden");
+        connectUserWallet.textContent = account;
+        
+        //Instantiate contract instance
+        contractInstance = new web3.eth.Contract(ABI, CONTRACT);
+    } else {
+        alert('No metamask installed!')
+    }
+}
+connectMetamask.addEventListener("click", connectToMetamask);
+
