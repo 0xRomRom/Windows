1bd48d39699f0cc89a2eda0f60b30d15f45dba76
0xRomRom <roman0811@live.nl>
2023-04-08 15:32:19 +0200

    Added focus to input of terminal

--- js/terminal.js
+++ js/terminal.js
@@ -1,57 +1,56 @@
-
 const terminalInput = document.querySelector(".terminal-input");
 const terminalOutput = document.querySelector(".terminal-output");
-
+const terminalBody = document.querySelector(".terminal-body");
+terminalBody.addEventListener("click", () => {
+  terminalInput.focus();
+});
 
 const prePend = `<span class="terminal-prompt-user">0x398fa</span>
 <span class="terminal-prompt-path">~ $</span>`;
-
 
 terminalInput.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
     const command = e.target.value;
     e.target.value = "";
-    if (command === '' || command === " ") return;
+    if (command === "" || command === " ") return;
 
-    if (command === 'dn') {
-        const output = document.createElement("div");
-        output.style.color = 'green';
-        output.innerHTML = prePend;
-        output.textContent = "Suck deez nutsssss";
-        terminalOutput.insertAdjacentElement("beforeend", output);
-        return; 
+    if (command === "dn") {
+      const output = document.createElement("div");
+      output.style.color = "green";
+      output.innerHTML = prePend;
+      output.textContent = "Suck deez nutsssss";
+      terminalOutput.insertAdjacentElement("beforeend", output);
+      return;
     }
-    
-    if (command === 'funsu') {
-        const output = document.createElement("div");
-        
-        output.innerHTML = prePend;
-        output.textContent = command;
-        
-        output.style.color = 'red';
-        output.innerHTML = prePend;
-        output.textContent = "Funsu is gonu";
-        terminalOutput.insertAdjacentElement("beforeend", output);
-        return; 
+
+    if (command === "funsu") {
+      const output = document.createElement("div");
+
+      output.innerHTML = prePend;
+      output.textContent = command;
+
+      output.style.color = "red";
+      output.innerHTML = prePend;
+      output.textContent = "Funsu is gonu";
+      terminalOutput.insertAdjacentElement("beforeend", output);
+      return;
     }
 
     const output = document.createElement("div");
-    output.style.color = 'red';
+    output.style.color = "red";
     output.innerHTML = prePend;
     output.textContent = "Unknown command: " + command;
     terminalOutput.insertAdjacentElement("beforeend", output);
-    return; 
+    return;
   }
 });
 
-
 const spinner = document.querySelector(".spinner");
-
 
 const rotateSpinner = () => {
   let index = 0;
   setInterval(() => {
-    if(index === 8) {
+    if (index === 8) {
       index = 0;
     }
     spinner.innerHTML = spinElements[index];
@@ -59,6 +58,5 @@ const rotateSpinner = () => {
   }, 100);
 };
 rotateSpinner();
-
 
 const spinElements = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
