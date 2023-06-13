78938f5a229607efd98b76c2b2024095b6e2927e
Roman Van der Sar <r.van.der.sar@connectingthedots.nl>
2023-06-13 19:24:32 +0200

    Contracts added

--- EVM/SCRSLottery.sol
+++ EVM/SCRSLottery.sol
@@ -3,6 +3,7 @@
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";
 
 pragma solidity ^0.8.0;
 
@@ -10,7 +11,7 @@ contract SCRSLottery is Ownable {
     using SafeERC20 for IERC20;
 
     IERC20 public SCRSToken;
-
+    IERC721 public SCRSNFT;
 
     uint public ENTRY_FEE;
     uint public lotteryPot;
@@ -20,7 +21,6 @@ contract SCRSLottery is Ownable {
     constructor(address _SCRSTokenAddress) {
         SCRSToken = IERC20(_SCRSTokenAddress);
         ENTRY_FEE = 100000 * 10 ** 18;
-        lotteryActive = true;
     }
 
     function getEntrantsCount() public view returns (uint) {
@@ -37,12 +37,21 @@ contract SCRSLottery is Ownable {
 
     function fundLotteryPot(uint _amount) public onlyOwner {
         lotteryPot += _amount;
+        lotteryActive = true;
     }
 
     function enterLottery(address _player, uint _entryCount) public payable {
         uint entryCost = _entryCount * ENTRY_FEE;
-        require(SCRSToken.balanceOf(_player) >= entryCost, "Not enough $SCRS tokens to participate");
-        require(lotteryActive, "Wait for lottery start");
+        // uint nftBalance = SCRSNFT.balanceOf(_player);
+        uint tokenBalance = SCRSToken.balanceOf(_player);
+        
+        // require(nftBalance > 0, "Only for Sicarius holders!");
+
+        require(tokenBalance >= entryCost, "Not enough $SCRS to participate");
+        // require(lotteryActive, "Wait for lottery start");
+
+        uint256 allowance = SCRSToken.allowance(_player, address(this));
+        require(allowance >= entryCost, "Check the token allowance");
 
         lotteryPot += entryCost;
 
@@ -50,15 +59,15 @@ contract SCRSLottery is Ownable {
             lotteryEntrants.push(_player);
         }
 
-        SCRSToken.safeTransferFrom(_player, address(this), entryCost);
+        SCRSToken.transferFrom(_player, address(this), entryCost);
     }
     
     function drawWinner(uint index) public onlyOwner {
         uint randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % index;
         address winner = lotteryEntrants[randomNumber];
-        SCRSToken.transfer(winner, lotteryPot);
+        SCRSToken.transfer(payable(winner), lotteryPot);
         lotteryPot = 0;
         lotteryEntrants = new address[](0);
-
+        lotteryActive = false;
     }
 }
