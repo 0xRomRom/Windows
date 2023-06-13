d16681a9809adab5d15781db3235a49f8785869d
Roman Van der Sar <r.van.der.sar@connectingthedots.nl>
2023-06-13 16:46:54 +0200

    New changes

--- EVM/SCRSLottery.sol
+++ EVM/SCRSLottery.sol
@@ -6,32 +6,59 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 pragma solidity ^0.8.0;
 
-contract SCRSLottery is Ownable{
- using SafeERC20 for IERC20;
+contract SCRSLottery is Ownable {
+    using SafeERC20 for IERC20;
 
- IERC20 public SCRSToken;
- 
- address[] public lotteryParticipants;
+    IERC20 public SCRSToken;
 
- uint public ENTRY_FEE;
- uint public lotteryPot;
 
- constructor(address _SCRSTokenAddress) {
-    SCRSToken = _SCRSTokenAddress;
-    ENTRY_FEE = 100000 * 10 ** 18;
- }
+    uint public ENTRY_FEE;
+    uint public lotteryPot;
+    address[] public lotteryEntrants;
+    bool public lotteryActive;
 
- function changeEntryFee(uint _newFeeAmount) public onlyOwner {
-    ENTRY_FEE = _newFeeAmount;
- }
+    constructor(address _SCRSTokenAddress) {
+        SCRSToken = IERC20(_SCRSTokenAddress);
+        ENTRY_FEE = 100000 * 10 ** 18;
+        lotteryActive = true;
+    }
 
- function getContractSCRSBalance() public view returns (uint) {
-    return SCRSToken.balanceOf(address(this));
- }
+    function getEntrantsCount() public view returns (uint) {
+        return lotteryEntrants.length;
+    }
 
- function fundLotteryPot(uint _amount) public onlyOwner {
-    lotteryPot += _amount;
- }
+    function changeEntryFee(uint _newFeeAmount) public onlyOwner {
+        ENTRY_FEE = _newFeeAmount;
+    }
 
- 
+    function getContractSCRSBalance() public view returns (uint) {
+        return SCRSToken.balanceOf(address(this));
+    }
+
+    function fundLotteryPot(uint _amount) public onlyOwner {
+        lotteryPot += _amount;
+    }
+
+    function enterLottery(address _player, uint _entryCount) public payable {
+        uint entryCost = _entryCount * ENTRY_FEE;
+        require(SCRSToken.balanceOf(_player) >= entryCost, "Not enough $SCRS tokens to participate");
+        require(lotteryActive, "Wait for lottery start");
+
+        lotteryPot += entryCost;
+
+        for(uint i = 0; i < _entryCount; i++) {
+            lotteryEntrants.push(_player);
+        }
+
+        SCRSToken.safeTransferFrom(_player, address(this), entryCost);
+    }
+    
+    function drawWinner(uint index) public onlyOwner {
+        uint randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % index;
+        address winner = lotteryEntrants[randomNumber];
+        SCRSToken.transfer(winner, lotteryPot);
+        lotteryPot = 0;
+        lotteryEntrants = new address[](0);
+
+    }
 }
